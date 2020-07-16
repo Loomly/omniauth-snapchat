96fed53ce461dac88cb2a36c191739c9a5b364e7
Ari Summer <aribsummer@gmail.com>
2020-07-16 16:19:01 -0600

    Cleanup syntax and spacing

--- lib/omniauth/strategies/snapchat.rb
+++ lib/omniauth/strategies/snapchat.rb
@@ -8,29 +8,29 @@ module OmniAuth
       option :name, "snapchat"
 
       option :client_options, {
-        :site          => 'https://adsapi.snapchat.com',
-        :authorize_url => 'https://accounts.snapchat.com/login/oauth2/authorize',
-          :token_url => 'https://accounts.snapchat.com/accounts/oauth2/token'
+        site: "https://adsapi.snapchat.com",
+        authorize_url: "https://accounts.snapchat.com/login/oauth2/authorize",
+        token_url: "https://accounts.snapchat.com/accounts/oauth2/token"
       }
 
-      uid{ raw_info['me']['externalId'] }
+      uid { raw_info["me"]["externalId"] }
 
       info do
         {
-          name: raw_info['me']['displayName'],
-          image: raw_info['me']['bitmoji']['avatar']
+          name: raw_info["me"]["displayName"],
+          image: raw_info["me"]["bitmoji"]["avatar"]
         }
       end
 
       extra do
         {
-          'raw_info' => raw_info
+          "raw_info" => raw_info
         }
       end
 
+      # After back and forth with snapchat support team, using the GraphQL
+      # endpoint seems the easiest.
       def raw_info
-        # After back and forth with snapchat support team, using the GraphQL
-        # endpoint seems the easiest.
         raw_info_url = "https://kit.snapchat.com/v1/me?query=%7Bme%7BexternalId%2C+displayName%2C+bitmoji%7Bavatar%7D%7D%7D"
         @raw_info ||= access_token.get(raw_info_url).parsed["data"]
       end
@@ -42,10 +42,10 @@ module OmniAuth
       def token_params
         authorization = Base64.strict_encode64("#{options.client_id}:#{options.client_secret}")
         super.merge({
-                        headers: {
-                            'Authorization' => "Basic #{authorization}"
-                        }
-                    })
+          headers: {
+            "Authorization" => "Basic #{authorization}"
+          }
+        })
       end
     end
   end
